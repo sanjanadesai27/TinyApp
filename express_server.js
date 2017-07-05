0d5b057a8ecbf4a7d6357e1792dc168a8058e231
Sanjana Desai <sanjana_desai@live.ca>
2017-07-05 04:17:20 +0000

    created views (template) files

--- express_server.js
+++ express_server.js
@@ -1,7 +1,38 @@
 const express = require('express');
 const app = express();
-
 const PORT = process.env.PORT || 8080;
 
 app.set("view engine", "ejs");
 
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+}
+
+app.get('/', (request,response)=> {
+  response.json(urlDatabase);
+});
+
+app.get('/urls', (request, response) => {
+  let templateVars = { urls: urlDatabase };
+  response.render('urls_index', templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id,
+                        longURL: urlDatabase[req.params.id]
+                     };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/urls.json",(request, response) => {
+  response.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
