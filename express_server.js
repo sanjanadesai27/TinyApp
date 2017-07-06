72024ddf2489dc260f8bfcb297852cec7e1834a0
Sanjana Desai <sanjana_desai@live.ca>
2017-07-06 23:33:13 +0000

    created feature by which user can register

--- express_server.js
+++ express_server.js
@@ -14,8 +14,21 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 }
 
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+}
+
 app.use(function(req, res, next){
-  res.locals.username = req.cookies.username;
+  res.locals.user = users[req.cookies.user_id];
   next();
 });
 
@@ -26,6 +39,35 @@ function generateRandomString() {
         text += possible.charAt(Math.floor(Math.random() * possible.length));
     }
     return text;
+}
+
+function checkUser(email) {
+  // return !!(Object.values(users).find(user=>user.email === email);
+  for(id in users) {
+    if(email == users[id].email) {
+      return true;
+    }
+  }
+  return false;
+}
+// function getUserId(email) {
+//   // return !!(Object.values(users).find(user=>user.email === email);
+//   for(id in users) {
+//     if(email == users[id].email) {
+//       return id;
+//     }
+//   }
+//   return undefined;
+// }
+
+function getUser(email) {
+  // return !!(Object.values(users).find(user=>user.email === email);
+  for(id in users) {
+    if(email == users[id].email) {
+      return users[id];
+    }
+  }
+  return undefined;
 }
 
 app.get('/', (request,response)=> {
@@ -44,6 +86,7 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id,
                         longURL: urlDatabase[req.params.id],
+                        email: response.locals.user.email
                      };
   res.render("urls_show", templateVars);
 });
@@ -66,19 +109,59 @@ app.post("/urls/:id", (request, response) => {
 });
 
 app.post("/login", (request,response) => {
-  response.cookie('username', request.body.username);
-  response.redirect("/urls")
+  //check if passwords match
+  //given email, return id
+  const  {email, password} = request.body;
+  const user = getUser(email);
+  if(!user) {
+    response.status(403).send("User does not exist. Please register")
+  } else {
+   savedPass = user.password;
+    if(password == savedPass) {
+      response.cookie('user_id', user.id);
+      response.redirect("/urls");
+    }
+    else {
+      response.redirect("/login");
+    }
+  }
 });
 
 app.get("/u/:shortURL", (req, res) => {
   let longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
+  return res.redirect(longURL);
 });
 
 app.post("/logout", (request, response) => {
-  response.clearCookie("username");
+  response.clearCookie("user_id");
   response.redirect("/urls");
 });
+
+app.get("/login",(request, response) => {
+  response.render("login");
+})
+
+app.get("/register", (request, response) => {
+  response.render("register");
+});
+
+app.post("/register", (request, response) => {
+  let randomString = generateRandomString();
+  const {email, password} = request.body;
+  if(!email || !password) {
+    response.status(400).send("Your email or password were missing!");
+  } else if(checkUser(email)) {
+    response.status(400).send("The email address you entered is already registered.");
+  } else {
+  users[randomString] = {
+    id : randomString,
+    email : request.body.email,
+    password : request.body.password
+  }
+  response.cookie('user_id', randomString);
+  response.redirect("/urls");
+}});
+
 
 app.listen(PORT, () => {
   console.log(`TinyApp server listening on port ${PORT}`);
