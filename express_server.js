ace6d49305ab4fc0f6acb8cccbc629d1536a1e80
Sanjana Desai <sanjana_desai@live.ca>
2017-07-08 00:07:34 +0000

    Updated server to hash passwords.

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 const cookieParser = require('cookie-parser');
-
+const bcrypt = require("bcrypt");
 app.set("view engine", "ejs");
 
 const bodyParser = require("body-parser");
@@ -18,7 +18,7 @@ const users = {
   "userRandomID": {
     id: "userRandomID",
     email: "user@example.com",
-    password: "purple-monkey-dinosaur"
+    password: "dinosaur",
   },
  "user2RandomID": {
     id: "user2RandomID",
@@ -146,17 +146,15 @@ app.post("/urls/:id", checkUserLogin, checkUserLink, (req, res) => {
 app.post("/login", (req,res) => {
   //check if passwords match
   //given email, return id
-  const  {email, password} = req.body;
+  const {email, password} = req.body;
   const user = getUser(email);
   if(!user) {
     res.status(403).send("User does not exist. Please register")
   } else {
-   savedPass = user.password;
-    if(password == savedPass) {
+    if (bcrypt.compareSync(password, user.hashedPassword)) {
       res.cookie('user_id', user.id);
       res.redirect("/urls");
-    }
-    else {
+    } else {
       res.redirect("/login");
     }
   }
@@ -189,19 +187,20 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
   let randomString = generateRandomString();
   const {email, password} = req.body;
-  if(!email || !password) {
+  if (!email || !password) {
     res.status(400).send("Your email or password were missing!");
-  } else if(checkUser(email)) {
+  } else if (checkUser(email)) {
     res.status(400).send("The email address you entered is already registered.");
   } else {
-  users[randomString] = {
-    id : randomString,
-    email : req.body.email,
-    password : req.body.password
+    users[randomString] = {
+      id : randomString,
+      email : email,
+      hashedPassword : bcrypt.hashSync(password, 10)
+    }
+    res.cookie('user_id', randomString);
+    res.redirect("/urls");
   }
-  res.cookie('user_id', randomString);
-  res.redirect("/urls");
-}});
+});
 
 
 app.listen(PORT, () => {
